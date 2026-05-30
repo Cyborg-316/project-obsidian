1126acc2a506e8af5f1f79da718364b83db6c748
Cyborg-316 <mr.jmbo1234@gmail.com>
2026-05-30 17:28:56 -0400

    mashed potatoes

--- project/main.py
+++ project/main.py
@@ -1,30 +1,67 @@
 #Neural Network with GPU and possibly TPU
-#version 1.0.1
+#version 1.0.2
 
 import numpy as np
 import math
 #import pygame
 import random
 import cupy as cp
+from mnist import MNIST
 
 def main():
     __onStart()
-    
-    y_true = cp.array([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
-    ])
 
-    y_pred = cp.array([
-        [0.7, 0.2, 0.1],
-        [0.1, 0.8, 0.1],
-        [0.2, 0.2, 0.6]
-    ])
-    print(mat_cross_entropy(y_true, y_pred))
+    mndata = MNIST(r"C:\Users\Anton\OneDrive\Documents\Repositories\project-obsidian\mnist")
+    
+    images, labels = mndata.load_training()
+    train_images = images[:50000]
+    train_labels = labels[:50000]
+    test_images = images[50000:]
+    test_labels = labels[50000:]
+
+    draw_mnist_digit(train_images[800])
+    print(num_to_vector(train_labels[800]))
     
     
     __onEnd()
+
+def init_parameters(numInputs, numOutputs, numNuerons, numHiddenLayers):
+    W1 = cp.random.randn(numNuerons, numInputs)
+    b1 = cp.random.randn(numNuerons, 1)
+    W2 = cp.random.randn(numNuerons, numNuerons)
+    b2 = cp.random.randn(numNuerons, 1)
+    return W1, b1, W2, b2
+
+def foward_prop(W1, b1, W2, b2, x):
+    Z1 = mat_add(mat_mult(W1, x, False, False), b1)
+    A1 = mat_relu(Z1)
+    Z2 = mat_add(mat_mult(W2, A1, False, False), b2)
+    A2 = mat_softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def back_propr(Z1, A1, Z2, A2):
+    pass
+
+def draw_mnist_digit(data):
+    if len(data) == 784:
+        for x in range(28):
+            print("|", end = "")
+            for y in range(28):
+                char = data[y + 28 * x]
+                if char > 0:
+                    print("0  ", end = "")
+                else:
+                    print("   ", end = "")
+            print("|")
+        return True
+    return False
+
+def num_to_vector(num):
+    if num <= 9 and num >= 0:
+        vector = cp.zeros(10)
+        vector[num] += 1
+        return vector.reshape(-1,1)
+    return False
 
 def mat_create(data, rows, columns):
     if len(data) == rows * columns:
@@ -56,7 +93,7 @@ def mat_add(matrix_a, matrix_b):
     if (matrix_a.shape == matrix_b.shape):
         return False
     
-    return matrix_a + matrix_b
+    return cp.add(matrix_a, matrix_b)
         
 
 def mat_sub(matrix_a, matrix_b):
@@ -105,8 +142,6 @@ def mat_cross_entropy(matrix_p, matrix_q):
 
 
 def __onStart():
-    global Network
-    Network = []
     print("------------------RUNNING------------------")
 
 def __onEnd():
