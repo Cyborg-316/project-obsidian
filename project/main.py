2f1e7eb0b43eaf8b7fce9b95da25a63d7b2adb26
Cyborg-316 <mr.jmbo1234@gmail.com>
2026-06-26 00:06:59 -0400

    fixing branches

--- project/main.py
+++ project/main.py
@@ -8,11 +8,14 @@ def main():
     input_array = cp.array(range(10))
     output_array = 2 * input_array + 1
     
-    NNM = network(1, 1, 1, 1)
+    NNM = network(1, 2, 3, 5)
+    print(NNM.weights, "\n\n\n", NNM.biases)
     NNM.feed(input_array, output_array)
     NNM.train(1000, 0.05)
 
-    draw_mnist_digit(7)
+    draw_mnist_digit(8)
+
+
 
 
 
@@ -24,6 +27,21 @@ class network():
         self.neuron_amt = c
         self.output_amt = d
 
+        #init weights & biases
+        weights = []
+        biases = []
+        if b > 0:
+            weights.append(cp.zeros((c, a)))
+            for i in range(b - 1):
+                weights.append(cp.zeros((c, c)))
+            for i in range(b):
+                biases.append(cp.zeros((c, 1)))
+            weights.append(cp.zeros((d, c)))
+            biases.append(cp.zeros((d, 1)))
+
+            self.weights = weights
+            self.biases = biases
+
     def train(self, iterations, lr):
         for epoch in range(iterations):
             #pass
@@ -34,6 +52,19 @@ class network():
     def feed(self, array_1, array_2):
         self.input_cache = array_1
         self.actual_cache = array_2
+
+    def foward(self):
+        #return weighted sums and activations
+        input_amt = self.input_amt
+        layer_amt = self.hidden_layer_amt
+        neuron_amt = self.neuron_amt
+        output_amt = self.output_amt
+
+        weightedSums = []
+        activations = []
+
+
+
 
 def draw_mnist_digit(number):
     mndata = MNIST('mnist')
